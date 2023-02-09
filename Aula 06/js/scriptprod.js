2aa7232c95f9e5066e6bc3c020da8f25ba2effdf
Uriel <uriel.mishima@bol.com.br>
2023-02-09 17:19:39 -0300

    Criação da página de produtos

--- Aula 06/js/scriptprod.js
+++ Aula 06/js/scriptprod.js
@@ -1,9 +1,28 @@
-window.document.head.innerHTML=`<link rel="stylesheet" href="css/styles02.css" />`;
+window.document.head.innerHTML = `<link rel="stylesheet" href="css/styles02.css" />`;
+
+const divCard = document.createElement('div');
+divCard.setAttribute('class', "divCard");
+
+const imgCard = document.createElement('img');
+imgCard.setAttribute('id', 'imgCard');
+divCard.appendChild(imgCard);
+
+const fechar = document.createElement('a');
+fechar.innerHTML = "[X] Fechar";
+fechar.setAttribute('class',"fechar");
+divCard.appendChild(fechar);
+
+fechar.onclick = function(){
+    document.getElementsByClassName("divCard")[0].style.height = "0vh";
+    document.getElementById("imgCard").style.opacity = "0";
+    
+}
+
 
 const body = window.document.body;
 
 
-function paginaProdutos(){
+function carregarProdutos(){
     
     const header = document.createElement('header');
     const h1 = document.createElement('h1');
@@ -12,31 +31,59 @@ function paginaProdutos(){
     
     const main = document.createElement('main');
 
-
     var i = 0; 
     while (i < listaProdutos.length){
-    var divProdutos = document.createElement('div');
-    var imgProdutos = document.createElement('img');
-    var h1Title = document.createElement('h1');
-    var h2Discr = document.createElement('h2')
-    var pdata = document.createElement('p');
-    
-    divProdutos.setAttribute('class','divProdutos');
-    imgProdutos.setAttribute('src');
-    imgProdutos.setAttribute('class','imgProduto');
-    h2Titulo.innerHTML = listaProdutos[i].title;
-    pLancamento.setAttribute('class', 'pdata');
-    pLancamento.innerHTML = listaProdutos[i].release_date;
-    
-    divProdutos.appendChild(imgProdutos);
-    divProdutos.appendChild(h2Titulo);
-    divProdutos.appendChild(pdata);
-    main.appendChild(divProdutos);
-    i++; 
-}
-    
 
+        var divProduto = document.createElement('div');
+        var imgProduto = document.createElement('img');
+        var h2Discr = document.createElement('h2');
+        var h3Marca = document.createElement('h3')
+        var pPreco = document.createElement('p');
+
+        divProduto.setAttribute('class', 'divProduto')
+        imgProduto.setAttribute("src", listaProdutos[i].thumbnail);
+        imgProduto.setAttribute("class", "imgProduto")
+
+    h2Discr.innerHTML = listaProdutos[i].title;
+    h3Marca.innerHTML = listaProdutos[i].brand;
+    pPreco.innerHTML = listaProdutos[i].price
+       
+    divProduto.appendChild(imgProduto);
+    divProduto.appendChild(h2Discr);
+    divProduto.appendChild(h3Marca);
+    divProduto.appendChild(pPreco);
+    main.appendChild(divProduto);
+
+    var divMiniatura = document.createElement("div");
+    divMiniatura.setAttribute("class", "divMiniatura");
+
+    var x = 0;
+    while( x < listaProdutos[i].images.length){
+        var imgMiniatura =  document.createElement("img");
+        imgMiniatura.setAttribute('src',listaProdutos[i].images[x]);
+        imgMiniatura.setAttribute("class", "imgMiniatura");
+        
+        imgMiniatura.addEventListener("click", exibirImagem.bind(this,listaProdutos[i].images[x]));
+        
+        
+        divMiniatura.appendChild(imgMiniatura);
+        x++;
+    }
+
+    divProduto.appendChild(divMiniatura);
+    main.appendChild(divProduto);
+    i++;
+
+    } 
+    body.appendChild(divCard);
     body.appendChild(header);
     body.appendChild(main);
 }
-body.addEventListener("load",paginaProdutos());
+function exibirImagem(foto){
+    document.getElementsByClassName("divCard")[0].style.height = "60vh";
+    document.getElementById("imgCard").src = foto;
+    document.getElementById("imgCard").style.opacity = "1";
+
+}
+
+body.addEventListener("load", carregarProdutos());
